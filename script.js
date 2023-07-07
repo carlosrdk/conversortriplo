b634ca4c220d852a79aa65af61674f80f2283d19
Letoshi <CarlosLima36_@outlook.com>
2023-07-07 12:01:17 -0300

    Adicionando conversor de decimal para binário para hexadecimal

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function decimalBinarioHexadecimal() {
+    let valorDecimal = document.getElementById("decimal").value;
+    let valorBinario = parseInt(valorDecimal).toString(2);
+    let valorHexadecimal = parseInt(valorDecimal).toString(16).toUpperCase();
 
+    document.getElementById("binario").value = valorBinario;
+    document.getElementById("hexadecimal").value = valorHexadecimal;
+}
